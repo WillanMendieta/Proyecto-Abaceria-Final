37a66e9ce7a4389c84de7843fdab7355b4f571f7
steve <steve@DESKTOP-6MGVTID>
2019-07-04 01:14:57 -0500

    ok

--- src/modelo/Main.java
+++ src/modelo/Main.java
@@ -16,7 +16,7 @@ public class Main {
     public static void main(String[] args) {
         
         
-        System.out.println("hiiiiiiiiiiii   ");
+        System.out.println("Tonto quien es el papa  ");
         
 
              try{
