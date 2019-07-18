eeb55bc2dab4179a54fb60b4c7de2f7bc1d89ab8
Macao <Macao@172.16.210.14>
2019-07-18 13:54:24 -0500

    Creacion de la clase Main para la ejecucion del programa

---
+++ src/Main/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import Vista.VntLogin;
+
+/**
+ *
+ * @author Macao
+ */
+public class Main {
+    public static void main(String[] args) {
+        VntLogin  login=new VntLogin();
+             login.setVisible(true);
+        
+    }
+    
+}
