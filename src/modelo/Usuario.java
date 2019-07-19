633e067fe71ca9cf7e384f609b6305c97fe683da
Macao <Macao@172.16.210.14>
2019-07-19 17:22:30 -0500

    Creacion de nuevas clases en el paquete modelo (Cliente, Persona, Proveedor, Usuario)

---
+++ src/modelo/Usuario.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Macao
+ */
+public class Usuario extends Persona{
+    
+    private String cargo;
+    private String usuario;
+    private String contraseña;
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "cargo=" + cargo + ", usuario=" + usuario + ", contrase\u00f1a=" + contraseña + '}';
+    }
+    
+    
+    
+}
