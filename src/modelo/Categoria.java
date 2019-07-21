f762555ea5a09ca9ea97e70ab2e0e940822debb6
Macao <Macao@172.16.210.14>
2019-07-21 05:17:32 -0500

    crecion de la clase categoria para el almacenamiento de los datos de cada categoria

---
+++ src/modelo/Categoria.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Macao
+ */
+public class Categoria {
+    
+    private int id;
+    private String nombre;
+    private String estado;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" + "id=" + id + ", nombre=" + nombre + ", estado=" + estado + '}';
+    }
+    
+    
+}
