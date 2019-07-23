4b5116b82e9aaf2db1a51eaca105b3ac0321563b
Macao <Macao@172.16.210.14>
2019-07-23 06:15:07 -0500

    agregacion de nuevos metodos para la actualizacion de datos tanto de empleado como de cliente

--- src/Controlador/ControlPersona.java
+++ src/Controlador/ControlPersona.java
@@ -10,6 +10,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import javax.swing.JTextField;
 import modelo.*;
 
 
@@ -40,7 +41,6 @@ public class ControlPersona {
     
      public Usuario buscarEmpleado(conexionDB con, String usuario, String pass ){
          Usuario user = new Usuario();
-         Persona per = new Persona();
         try {
             
             System.out.println("usuario llega metodo :" + usuario + " " + pass);
@@ -161,6 +161,146 @@ public class ControlPersona {
             e.printStackTrace();
         }
         
+    }
+
+    public Cliente buscarCliente(conexionDB con, String jTextCedula, String jTextNombre) {
+        Cliente cli = new Cliente();
+        try {
+            psentencia=con.getConexion().prepareStatement("SELECT *"+ 
+                "FROM aba_personas " +
+               "WHERE per_cedula=?" +
+               " or per_nombre=? "
+            );
+            psentencia.setString(1, jTextCedula );
+            psentencia.setString(2, jTextNombre);
+            resultado = psentencia.executeQuery();
+            //Se presenta el resultado
+            while (resultado.next()) {
+                cli = new Cliente();
+                cli.setId(resultado.getInt("per_id"));
+                cli.setCedula(resultado.getString("per_cedula"));
+                cli.setNombre(resultado.getString("per_nombre"));
+                cli.setApellido(resultado.getString("per_apellido"));
+                cli.setDireccion(resultado.getString("per_direccion"));
+                cli.setCargo(resultado.getString("per_cargo"));
+                cli.setTelefonoConvencional(resultado.getString("per_tel_convencional"));
+                cli.setTelefonoCelular(resultado.getString("per_tel_celular"));
+                cli.setEstado(resultado.getString("per_estado"));       
+                
+                //System.out.println("consultando datos del la clase usuario : " + user );
+               // System.out.println("\nconsultando datos de la classe persona : " + per);
+}
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return cli;
+        
+    }
+
+    public void actualizarDatosCliente(conexionDB con, int id, String cedula, String nombre, String apellido, String direccion, String convencional, String celular) {
+       
+         try {
+            String sentenciaInsercion = "update aba_personas set "
+                    + "per_cedula =?,"
+                    + "per_nombre=?,"
+                    + "per_apellido=?,"
+                    + "per_direccion=?,"
+                    + "per_tel_convencional=?,"
+                    + "per_tel_celular=?"
+                    + " where per_id=?";
+            psentencia = con.getConexion().prepareStatement(sentenciaInsercion);
+            psentencia.setString(1, cedula);
+            psentencia.setString(2, nombre);
+            psentencia.setString(3, apellido);
+            psentencia.setString(4, direccion);
+            psentencia.setString(5, convencional);
+            psentencia.setString(6, celular);
+            psentencia.setInt(7, id);
+            
+            
+            
+            
+            //Se ejecuta la sentencia de INSERT
+            psentencia.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    
+    
+    }
+
+    public Usuario buscarEmpleadoDatos(conexionDB con, String cedula, String nombre) {
+        Usuario usuarioDatos = new Usuario();
+        try {
+            psentencia=con.getConexion().prepareStatement("SELECT *"+ 
+                "FROM aba_personas " +
+               "WHERE per_cedula=?" +
+               " or per_nombre=? "
+            );
+            
+            psentencia.setString(1, cedula );
+            psentencia.setString(2, nombre);
+            resultado = psentencia.executeQuery();
+            //Se presenta el resultado
+            while (resultado.next()) {
+                usuarioDatos = new Usuario();
+                usuarioDatos.setId(resultado.getInt("per_id"));
+                usuarioDatos.setCedula(resultado.getString("per_cedula"));
+                usuarioDatos.setNombre(resultado.getString("per_nombre"));
+                usuarioDatos.setApellido(resultado.getString("per_apellido"));
+                usuarioDatos.setDireccion(resultado.getString("per_direccion"));
+                usuarioDatos.setUsuario(resultado.getString("per_usuario"));
+                usuarioDatos.setContraseña(resultado.getString("per_password"));
+                usuarioDatos.setCargo(resultado.getString("per_cargo"));
+                usuarioDatos.setTelefonoConvencional(resultado.getString("per_tel_convencional"));
+                usuarioDatos.setTelefonoCelular(resultado.getString("per_tel_celular"));
+                usuarioDatos.setEstado(resultado.getString("per_estado"));       
+                
+                //System.out.println("consultando datos del la clase usuario : " + user );
+               // System.out.println("\nconsultando datos de la classe persona : " + per);
+}
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return usuarioDatos;
+    }
+
+    public void actualizarDatosEmpleado(conexionDB con, int id, String cedula, String nombre, String apellido, String direccion, String convencional, String celular, String usuario, String contra, String cargo) {
+        try {
+            String sentenciaInsercion = "update aba_personas set "
+                    + "per_cedula =?,"
+                    + "per_nombre=?,"
+                    + "per_apellido=?,"
+                    + "per_direccion=?,"
+                    + "per_tel_convencional=?,"
+                    + "per_tel_celular=?,"
+                    + "per_usuario=?,"
+                    + "per_password=?,"
+                    + "per_cargo=?"
+                    + " where per_id=?";
+            psentencia = con.getConexion().prepareStatement(sentenciaInsercion);
+            psentencia.setString(1, cedula);
+            psentencia.setString(2, nombre);
+            psentencia.setString(3, apellido);
+            psentencia.setString(4, direccion);
+            psentencia.setString(5, convencional);
+            psentencia.setString(6, celular);
+            psentencia.setString(7, usuario);
+            psentencia.setString(8, contra);
+            psentencia.setString(9, cargo);
+            psentencia.setInt(10, id);
+            //Se ejecuta la sentencia de INSERT
+            psentencia.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    
+    
+    
+    
+    
     }
     
     }
