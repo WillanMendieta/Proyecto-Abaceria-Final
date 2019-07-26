704f46ca2f92b47a0c44e7c3d5e1bb09d0fec7d3
ASUS <jannematute_@hotmail.com>
2019-07-25 19:55:15 -0500

    Creacion de la clase registro compra junto con su getters y setters

---
+++ src/modelo/RegistrarCompra.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author ASUS
+ */
+public class RegistrarCompra {
+    private int id;
+    private String fechaIngreso;
+    private int numeroFactura;
+    private int cantidadProd;
+    private int idProveedores;
+    private int idProductos;
+    private Producto producto;
+    private Proveedor proveedor;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public void setFechaIngreso(String fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    public int getNumeroFactura() {
+        return numeroFactura;
+    }
+
+    public void setNumeroFactura(int numeroFactura) {
+        this.numeroFactura = numeroFactura;
+    }
+
+    public int getCantidadProd() {
+        return cantidadProd;
+    }
+
+    public void setCantidadProd(int cantidadProd) {
+        this.cantidadProd = cantidadProd;
+    }
+
+    public int getIdProveedores() {
+        return idProveedores;
+    }
+
+    public void setIdProveedores(int idProveedores) {
+        this.idProveedores = idProveedores;
+    }
+
+    public int getIdProductos() {
+        return idProductos;
+    }
+
+    public void setIdProductos(int idProductos) {
+        this.idProductos = idProductos;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public Proveedor getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(Proveedor proveedor) {
+        this.proveedor = proveedor;
+    }
+    
+    
+    
+    
+}
