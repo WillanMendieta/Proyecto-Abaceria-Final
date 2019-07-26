414fac9fbaeb01552881d3f1942e6c8604909930
ASUS <jannematute_@hotmail.com>
2019-07-25 20:05:44 -0500

    Creacion de los atributos en la clase Proveedores

--- src/modelo/Proveedor.java
+++ src/modelo/Proveedor.java
@@ -10,5 +10,37 @@ package modelo;
  * @author Macao
  */
 public class Proveedor {
-    
+    private int id;
+    private String ruc;
+    private String nombre;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRuc() {
+        return ruc;
+    }
+
+    public void setRuc(String ruc) {
+        this.ruc = ruc;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Proveedor{" + "id=" + id + ", ruc=" + ruc + ", nombre=" + nombre + '}';
+    }
 }
+
