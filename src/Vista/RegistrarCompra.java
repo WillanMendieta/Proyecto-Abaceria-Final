1831f1a6c39e73cc5ff6ab6f6a67a0b4fab51fed
jonathan Morocho <yuqui1tecnico@gmail.com>
2019-07-25 20:45:41 -0500

    Agregacion del metodo de regreso, instancia de los objetos de Usuario, ControlRegistro

--- src/Vista/RegistrarCompra.java
+++ src/Vista/RegistrarCompra.java
@@ -5,17 +5,26 @@
  */
 package Vista;
 
+import Conexion.*;
+import Controlador.*;
+import modelo.*;
+
 /**
  *
  * @author eika_
  */
 public class RegistrarCompra extends javax.swing.JFrame {
-
-    /**
-     * Creates new form RegistrarCompra
-     */
-    public RegistrarCompra() {
+    
+    Usuario user;
+    ControlRegistroCompras controlRegistro ;
+    conexionDB con = new conexionDB();
+    
+    
+    public RegistrarCompra( Usuario usuario) {
         initComponents();
+        user = usuario;
+        
+        controlRegistro = new ControlRegistroCompras();
     }
 
     /**
@@ -138,6 +147,11 @@ public class RegistrarCompra extends javax.swing.JFrame {
         Comprar.setText("Comprar");
 
         Cancelar.setText("Cancelar");
+        Cancelar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                CancelarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -174,7 +188,7 @@ public class RegistrarCompra extends javax.swing.JFrame {
                 .addComponent(Comprar)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(Cancelar)
-                .addGap(317, 317, 317))
+                .addGap(60, 60, 60))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -214,7 +228,7 @@ public class RegistrarCompra extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(Comprar)
                     .addComponent(Cancelar))
-                .addContainerGap(84, Short.MAX_VALUE))
+                .addContainerGap(23, Short.MAX_VALUE))
         );
 
         pack();
@@ -252,40 +266,14 @@ public class RegistrarCompra extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_BuscarNombreProActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(RegistrarCompra.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(RegistrarCompra.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(RegistrarCompra.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(RegistrarCompra.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+    private void CancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CancelarActionPerformed
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new RegistrarCompra().setVisible(true);
-            }
-        });
-    }
+        VntGestionPro pro = new VntGestionPro(user);
+        pro.setVisible(true);
+        setVisible(false);
+        
+    }//GEN-LAST:event_CancelarActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton BuscarCodigoPro;
